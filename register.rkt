ed050fe1761b6e21fac84f56e911e5448c435252
Benjamin Parra Luman <benjamin.parra.l@usach.cl>
2021-04-29 21:00:44 -0400

    Se ha creado la funcion register, aun faltn detalles

---
+++ register.rkt
@@ -0,0 +1,2 @@
+#lang racket
+(define (register socialnetwork date username password))
--- socialNetwork.rkt
+++ socialNetwork.rkt
@@ -1,15 +1,22 @@
 #lang racket
 ;TDA SOCIAL
+(require "tdaUsuario.rkt")
 (provide (all-defined-out))
 #||#
  ;(socialnetwork “fb” (date 25 10 2021) encryptFn encryptFn)
 
-(define (socialnetwork name date encFun decFun)
-         (define (redSocial name usuarios publicaciones)))
+#|(define (socialnetwork name date encFun decFun)
+         (lambda (redSocial name usuarios publicaciones)
+           (display "fuck")))|#
+(define (redSocial name usuarios publicaciones)
+  (if (or (equal? "facebook" name)(equal? "instagram" name)(equal? "twitter" name))
+      (if (map validaUsuario usuarios);crear tda publicacion para verificar si son del tipo y asi poder hacer un list user publicaciones
+          #t
+          #f)
+      #f)
+  )
 
-(define (creaPublicacion publicacion)
-  (if (= 5 (length publicacion))
-      ()))
+
   
 
 
